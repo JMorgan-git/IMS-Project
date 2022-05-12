367f65a078bd68d2fb531379bb78a5c1e243146b
Jesse <jesse.morgan@hotmail.co.uk>
2022-05-12 17:42:39 +0100

    adding exe

---
+++ target/classes/sql-data.sql
@@ -0,0 +1,10 @@
+-- INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+-- INSERT INTO `ims`.`items` ( `item_name`, `item_value`) VALUES ('apple', 1.5);
+ -- SELECT * FROM items;
+-- INSERT INTO `ims`.`orders` (`customer_id`) VALUES (1);
+-- INSERT INTO `ims`.`order_items` ( `order_id`, `item_id`) VALUES (1, 1);
+ -- SELECT * FROM ims.order_items WHERE `order_id` LIKE 3;
+-- SELECT * FROM ims.items;
+-- SELECT * FROM ims.order_items;
+ -- SELECT * FROM order_items WHERE order_id LIKE 1;
+  SELECT * FROM ims.customers;
---
+++ target/classes/sql-schema.sql
@@ -0,0 +1,35 @@
+-- drop schema ims;
+
+CREATE SCHEMA IF NOT EXISTS `ims`;
+
+USE `ims` ;
+
+CREATE TABLE IF NOT EXISTS `ims`.`customers` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(40) DEFAULT NULL,
+    `surname` VARCHAR(40) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`orders`(
+	`id` INT NOT NULL AUTO_INCREMENT,
+    `customer_id` INT NOT NULL,
+    FOREIGN KEY (`customer_id`) REFERENCES `ims`.`customers`(`id`)ON DELETE CASCADE,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`items`(
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`item_name` char(50) NOT NULL,
+    `item_value` DOUBLE NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`order_items`(
+	-- `id` INT NOT NULL AUTO_INCREMENT,
+    `order_id` INT NOT NULL,
+    `item_id` INT NOT NULL,
+    FOREIGN KEY (`order_id`) REFERENCES `ims`.`orders`(`id`)ON DELETE CASCADE,
+    FOREIGN KEY (`item_id`) REFERENCES `ims`.`items`(`id`)ON DELETE CASCADE
+    -- PRIMARY KEY (`id`)
+);
