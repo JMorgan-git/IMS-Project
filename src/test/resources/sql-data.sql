4d34ba911dd301a59bc87e63f5e9b25b29193f2e
Jesse <jesse.morgan@hotmail.co.uk>
2022-05-12 16:17:05 +0100

    added tests

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,1 +1,4 @@
-INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `customers` ( `first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `items` ( `item_name`, `item_value`) VALUES ('apple', 1.5);
+INSERT INTO `orders` (`customer_id`) VALUES (1);
+INSERT INTO `order_items` ( `order_id`, `item_id`) VALUES (1, 1);
--- src/test/resources/sql-schema.sql
+++ src/test/resources/sql-schema.sql
@@ -1,8 +1,34 @@
+DROP TABLE IF EXISTS `order_items`;
+DROP TABLE IF EXISTS `orders`;
+DROP TABLE IF EXISTS `items`;
 DROP TABLE IF EXISTS `customers`;
 
 CREATE TABLE IF NOT EXISTS `customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `id` INT NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
     PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `orders`(
+	`id` INT NOT NULL AUTO_INCREMENT,
+    `customer_id` INT NOT NULL,
+    FOREIGN KEY (`customer_id`) REFERENCES `customers`(`id`),
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `items`(
+	`id` INT NOT NULL AUTO_INCREMENT,
+	`item_name` char(50) NOT NULL,
+    `item_value` DOUBLE NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `order_items`(
+	-- `id` INT NOT NULL AUTO_INCREMENT,
+    `order_id` INT NOT NULL,
+    `item_id` INT NOT NULL,
+    FOREIGN KEY (`order_id`) REFERENCES `orders`(`id`),
+    FOREIGN KEY (`item_id`) REFERENCES `items`(`id`)
+    -- PRIMARY KEY (`id`)
 );
